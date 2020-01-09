f19fcf7f2d1a2d980c6b7fb1153e64f6966f5741
Ganesh <coolgimmy7@gmail.com>
2020-01-09 18:13:30 +0530

    Modified:comments programs

--- src/com/bridgelabz/felloship/inventory/JSONInventoryDataManagement.java
+++ src/com/bridgelabz/felloship/inventory/JSONInventoryDataManagement.java
@@ -1,36 +1,56 @@
 package com.bridgelabz.felloship.inventory;
 
-import java.io.IOException;
-import java.util.Scanner;
 import com.bridgelabz.felloship.operations.*;
 import com.bridgelabz.felloship.productmenu.*;
 
 public class JSONInventoryDataManagement {
 
-	@SuppressWarnings("resource")
-	public static void main(String[] args) throws IOException {
-		InventoryManager.pricemenu();
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("1: add\n" + "2: del\n" + "3: update\n" + "4: display\n" + "5: exit");
-		int key = scanner.nextInt();
-		switch (key) {
-		case 1:
-			operations.add();
-			break;
-		case 2:
-			operations.del();
-			break;
-		case 3:
-			operations.update();
-			break;
-		case 4:
-			operations.display();
-			break;
-		case 5:
-			return;
-		default:
-			break;
+	public static void main(String[] args) {
+		try {
+			menu();
+		} catch (Exception e) {
+			System.out.println(e);
 		}
+	}
+
+	// inventory show
+	public static void menu() {
+		InventoryManager.pricemenu();
+		System.out.println("******************Operations***************");
+
+		int key;
+		do {
+			System.out.println("1: Add Inventory\n" + "2: Remove Inventory\n" + "3: Update Inventory\n"
+					+ "4: Search Inventory\n5: Show all Inventory\n" + "6: Exit");
+			key = operations.scanner.nextInt();
+			switch (key) {
+			case 1:
+				InventoryManager.pricemenu(); // show inventory
+				operations.add(); // add new inventory
+				InventoryManager.pricemenu(); // show updated inventory
+				break;
+			case 2:
+				InventoryManager.pricemenu(); // show inventory
+				operations.del(); // remove inventory
+				InventoryManager.pricemenu(); // show updated inventory
+				break;
+			case 3:
+				InventoryManager.pricemenu(); // show inventory
+				operations.update(); // update inventory information
+				InventoryManager.pricemenu();// show updated inventory
+				break;
+			case 4:
+				operations.search(); // search inventry
+				break;
+			case 5:
+				InventoryManager.pricemenu(); // show all inventory with price
+				break;
+			case 6:
+				return;
+			default:
+				break;
+			}
+		} while (key != 6);
 	}
 
 }
--- src/com/bridgelabz/felloship/operations/operations.java
+++ src/com/bridgelabz/felloship/operations/operations.java
@@ -1,55 +1,52 @@
 package com.bridgelabz.felloship.operations;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.Scanner;
 
 import com.bridgelabz.felloship.control.control;
 import com.bridgelabz.felloship.inventory.Product;
-import com.fasterxml.jackson.core.JsonGenerationException;
-import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class operations {
 
-	public static String spath = "/home/admin1/Desktop/inventory.json";
-	static Scanner scanner = new Scanner(System.in);
+	public static Scanner scanner = new Scanner(System.in);
 	static ObjectMapper mapper = new ObjectMapper();
 
-	public static void del() throws IOException {
-		List<Product> list = control.read();
+	public static void del() {
+		List<Product> lists = control.readInventory(); // read all inventory
 		System.out.println("enter product name: ");
 		String inputname = scanner.next();
-		for (Product product : list) {
-			if (product.name.equals(inputname)) {
-				continue;
+		try {
+			for (Product product : lists) {
+				if (product.name.equals(inputname)) { // if found then
+					lists.remove(product); // remove inventory
+				}
 			}
-			list.add(product);
+		} catch (Exception e) {
 		}
-		control.write(list);
+		control.writeInventory(lists); // re-write updated inventory
 	}
 
-	public static void update() throws IOException {
-		List<Product> list = control.read();
+	public static void update() {
+		List<Product> list = control.readInventory(); // read all ininventory
 		System.out.println("enter product name: ");
 		String inputname = scanner.next();
 		for (Product product : list) {
 			if (product.name.equals(inputname)) {
-				System.out.println("1.product\n " + "2. price\n" + "3. weight");
+				System.out.println("1. Product Name\n" + "2. Price\n" + "3. Weight");
 				int choice = scanner.nextInt();
 				switch (choice) {
 				case 1:
 					System.out.println("new name");
-					product.setName(scanner.next());
+					product.setName(scanner.next()); // set new product name
 					break;
 				case 2:
 					System.out.println("new price");
-					product.setPrice(scanner.nextInt());
+					product.setPrice(scanner.nextInt()); // set new product price
 					break;
 				case 3:
 					System.out.println("change weight");
-					product.setWeight(scanner.nextInt());
+					product.setWeight(scanner.nextInt()); // set new product weight
 					break;
 				default:
 					break;
@@ -57,36 +54,38 @@ public class operations {
 			}
 
 		}
-		control.write(list);
+		control.writeInventory(list); // re-write updated inventory
 	}
 
-	public static void add() throws JsonGenerationException, JsonMappingException, IOException {
-
-		List<Product> array = control.read();
-		System.out.print("enter product sub types: ");
+	public static void add() {
+		List<Product> array = control.readInventory(); // read all inventory
+		System.out.println("enter the product"); // main product name
+		String product = scanner.next();
+		System.out.print("enter product sub types: "); // product sub types amount
 		int productsubtypes = scanner.nextInt();
 		for (int i = 0; i < productsubtypes; i++) {
-			Product p = new Product();
-			System.out.println("enter the name");
-			p.setName(scanner.next());
+			Product newproduct = new Product();
+			newproduct.setType(product); // set product type
+			System.out.println("enter the sub type" + (i + 1) + " product name");
+			newproduct.setName(scanner.next()); // set product name
 			System.out.println("enter the Weight: ");
-			p.setWeight(scanner.nextInt());
+			newproduct.setWeight(scanner.nextInt()); // set product weight
 
 			System.out.println("enter the price per Kg: ");
-			p.setPrice(scanner.nextInt());
+			newproduct.setPrice(scanner.nextInt()); // set product price
 
-			array.add(p);
+			array.add(newproduct); // add new product in list
 		}
-		control.write(array);
+		control.writeInventory(array); // re-write inventory list
 
 	}
 
-	public static void display() throws JsonParseException, JsonMappingException, IOException {
-		List<Product> list = control.read();
+	public static void search() {
+		List<Product> list = control.readInventory(); // read all inventory
 		System.out.println("enter the product");
 		String input = scanner.next();
 		for (Product product : list) {
-			if (product.name.equals(input)) {
+			if (product.name.equals(input)) { // if product found then show details
 				System.out.print("the total price is: ");
 				System.out.println(product.price * product.weight + " for " + product.weight + " kg");
 			}
